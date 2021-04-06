1f300e550fbfcd9e91eda3bc2b5219cdd9f4d783
John Collins <johncollins@Johns-Air.attlocal.net>
2021-04-05 19:19:35 -0500

    finished steps 4-7

--- src/app/orbit-list/orbit-list.component.ts
+++ src/app/orbit-list/orbit-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Satellite } from '../satellite';
 
 
@@ -15,5 +15,17 @@ export class OrbitListComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  sort(column: string): void {
+    // array.sort modifies the array, sorting the items based on the given compare function
+    this.satellites.sort(function(a: Satellite, b: Satellite): number {
+       if(a[column] < b[column]) {
+          return -1;
+       } else if (a[column] > b[column]) {
+          return 1;
+       }
+       return 0;
+    });
+ }
 
 }
